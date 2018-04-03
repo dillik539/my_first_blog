d0603df500f2e93f63c1517c5087c3b2d2f3046d
Dilli Khatiwoda <dilli@dilli.com>
2018-04-03 10:42:45 -0500

    Added the ability to add comments by the user, and the ability to remove or approve comment by the admin user.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 '''this makes the model visible in the admin page, and provides all the
 administrative accesses like edit, delete, add and so on'''
 admin.site.register(Post)
+
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,9 @@ from django.utils import timezone
 
 '''defines model object that will be eventually added to database with
 properties defined below as column headings.'''
+
 class Post(models.Model):
+    '''Define Post model'''
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -18,4 +20,20 @@ class Post(models.Model):
     '''when this function is called, it will publish the post model with title.'''
     def __str__(self):
         return self.title
-# Create your models here.
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name = 'comments')
+    author = models.CharField(max_length = 200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default = timezone.now)
+    approved_comment = models.BooleanField(default = False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+def approved_comments(self):
+    return self.comments.filter(approved_comment = True)
